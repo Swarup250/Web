aa5283858fcedb84b30676fb7773db881992215d
Swarup250 <ushature923@gmail.com>
2025-09-16 22:01:47 +0530

    Small changes

--- dashboard/src/Component/ToDoList.jsx
+++ dashboard/src/Component/ToDoList.jsx
@@ -21,7 +21,7 @@ function ToDoList() {
         setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo , completed : !prevTodo.completed}: prevTodo))
     }
     return (
-    <ToDoProvider value={{Todos, addTodo, updateTodo, deleteTodo, toggleCompelete }}>
+    <ToDoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleCompelete }}>
     <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">
