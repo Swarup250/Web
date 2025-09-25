3777174789fc299279c3009073610e4e6df29bcf
Swarup250 <ushature923@gmail.com>
2025-09-25 10:07:23 +0530

    Now the upadeteTodo is created

--- Redux/src/Feature/Todo/TodoSlice.js
+++ Redux/src/Feature/Todo/TodoSlice.js
@@ -2,11 +2,33 @@ import { createSlice ,nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
     todos:[
-        {id:1, text:"Hello world"}
+        {id:1, text:"Hello world"}//state
     ]
 }
+//Action value 
+// {type : "todo/addTodo(fuction)" , payload : hello world}
 
 export const todoSlice = createSlice({
     name:"Todo",
     initialState,
+    reducers :{
+        addTodo : (state ,action) =>{
+            const todo = {
+                id:nanoid(),// this is used for  payload.id and the state.id
+                text : action.payload.text,
+            };
+            state.todos.push(todo);
+        },//also we get state and action it is syntax
+        removeTodo : (state,action) =>{
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        upadateTodo : (state ,action) =>{
+            state.todos = state.todos.map((todo) => 
+            todo.id === action.payload.id 
+            ? {...todo,text : action.payload.text,}
+            :todo
+            )
+        },
+        deleteTodo
+    }
 })
