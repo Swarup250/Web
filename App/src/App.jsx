00986e4791d31f369ecdc538b6410004e0fa8f12
Swarup250 <ushature923@gmail.com>
2025-10-05 16:48:43 +0530

    just testing grid

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -4,7 +4,21 @@ import './App.css'
 function App() {
   console.log(config)
   return (
-    <div>Hello World</div>
+    <div className='h-screen w-screen'>
+      <div className='text-amber-300 grid '>This an test for Grid
+      <div className=' bg-amber-400 grid grid-cols-3 gap-5 justify-items-center'>
+        <div className="bg-amber-600 h-40 w-40 "></div>
+        <div className="bg-amber-600 h-40 w-40"></div>
+        <div className="bg-amber-600 h-40 w-40"></div>
+        <div className="bg-amber-600 h-40 w-40"></div>
+        <div className="bg-amber-600 h-40 w-40"></div>
+        <div className="bg-amber-600 h-40 w-40"></div>
+        <div className="bg-amber-600 h-40 w-40"></div>
+        <div className="bg-amber-600 h-40 w-40"></div>
+        <div className="bg-amber-600 h-40 w-40"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
