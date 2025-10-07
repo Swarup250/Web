d8ab7ed90fe90f633b475e6229da5983c4551da6
Swarup250 <ushature923@gmail.com>
2025-10-07 17:30:00 +0530

    createAccount is done

--- App/appwrite/auth.js
+++ App/appwrite/auth.js
@@ -1,2 +1,31 @@
 import conf from "../appwrite/conf.js"
+import {Client, Account, ID} from "appwrite"
 
+export class AuthService {
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+            this.account = new Account(this.client)
+    }
+}
+
+async function createAccount({email ,password ,name}) {
+    try{
+        const userAccount = this.account.create(ID.unique(),email,password,name)
+        if(userAccount){
+            return 
+        }
+        else{
+            return userAccount;
+        }
+    }
+    catch(error){
+        return error;
+    }
+}
+
+const authService = new AuthService()
+export default authService
