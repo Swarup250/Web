cbc785e00423d2db5a4c328595717a1f2d7b6480
Swarup250 <ushature923@gmail.com>
2025-10-17 13:12:34 +0530

    revise

--- App/appwrite/config.js
+++ App/appwrite/config.js
@@ -1,2 +1,14 @@
 import conf from "./config.js"
-import {Client, ID} from "appwrite"
+import {Client, ID, Databases ,Storage ,Query} from "appwrite"
+
+export class Service{
+    client = new Client(),
+    databases,
+    bucket,
+    constructor(){
+        this.client
+            .setEndpoint
+    }
+}
+const service = new Service()
+export default service 
