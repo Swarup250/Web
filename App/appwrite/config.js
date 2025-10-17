5b4514ce79b831e13b0421b5b8576f524b2bc406
Swarup250 <ushature923@gmail.com>
2025-10-17 13:25:52 +0530

    createPost is done

--- App/appwrite/config.js
+++ App/appwrite/config.js
@@ -7,7 +7,22 @@ export class Service{
     bucket,
     constructor(){
         this.client
-            .setEndpoint
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+            this.databases = new Databases(this.client);
+            this.bucket = new Storage(this.client);
+    }
+    async createPost({title,slug,content,FearturedImage,status,userId}){
+        try {
+            await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {title,content,FearturedImage,status,userId}
+            )
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 const service = new Service()
